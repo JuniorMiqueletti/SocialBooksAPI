b694fac6c336035bdf49bdef8b36a1a4d761f379
Junior Miqueletti <juniormiqueletti@gmail.com>
2016-11-02 19:38:11 -0200

    Adjusting responses to follow HTTP RFC terms

--- src/main/java/br/com/juniormiqueletti/socialbooks/resources/BookResource.java
+++ src/main/java/br/com/juniormiqueletti/socialbooks/resources/BookResource.java
@@ -3,8 +3,13 @@ package br.com.juniormiqueletti.socialbooks.resources;
 import br.com.juniormiqueletti.socialbooks.domain.Book;
 import br.com.juniormiqueletti.socialbooks.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,28 +20,61 @@ public class BookResource {
     private BookRepository bookRepository;
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<Book> list(){
-        return bookRepository.findAll();
+    public ResponseEntity<List<Book>> list(){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(bookRepository.findAll());
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public void save(@RequestBody Book book){
+    public ResponseEntity save(@RequestBody Book book){
         bookRepository.save(book);
+
+        URI uri = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(book.getId())
+                            .toUri();
+
+        return ResponseEntity
+                .created(uri)
+                    .build();
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
-    public Book find(@PathVariable("id") Long id){
-        return bookRepository.findOne(id);
+    public ResponseEntity find(@PathVariable("id") Long id){
+        Book book = bookRepository.findOne(id);
+        if(book == null){
+            return ResponseEntity
+                    .notFound()
+                        .build();
+        }
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                    .body(book);
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable("id") Long id){
-        bookRepository.delete(id);
+    public ResponseEntity delete(@PathVariable("id") Long id){
+        try{
+            bookRepository.delete(id);
+        }catch (EmptyResultDataAccessException e){
+            return ResponseEntity
+                    .notFound()
+                        .build();
+        }
+        return ResponseEntity
+                .noContent()
+                    .build();
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.PUT)
-    public void update(@RequestBody Book book, @PathVariable("id") Long id){
+    public ResponseEntity update(@RequestBody Book book, @PathVariable("id") Long id){
         book.setId(id);
         bookRepository.save(book);
+
+        return ResponseEntity
+                .noContent()
+                    .build();
     }
 }
