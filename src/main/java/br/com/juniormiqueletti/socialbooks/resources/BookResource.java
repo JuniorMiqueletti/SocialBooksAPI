f1c40442e152cfd172c99cbcaf1da28e9124d2b9
Junior Miqueletti <juniormiqueletti@gmail.com>
2016-11-02 18:06:06 -0200

    add save into controller

--- src/main/java/br/com/juniormiqueletti/socialbooks/resources/BookResource.java
+++ src/main/java/br/com/juniormiqueletti/socialbooks/resources/BookResource.java
@@ -3,6 +3,7 @@ package br.com.juniormiqueletti.socialbooks.resources;
 import br.com.juniormiqueletti.socialbooks.domain.Book;
 import br.com.juniormiqueletti.socialbooks.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/books")
 public class BookResource {
 
     @Autowired
     private BookRepository bookRepository;
 
-    @RequestMapping(value = "books",method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public List<Book> list(){
         return bookRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void save(@RequestBody Book book){
+        bookRepository.save(book);
     }
 }
