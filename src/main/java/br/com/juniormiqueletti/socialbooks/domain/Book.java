ddbe57156d0816aaee4c66df21c82083deae94f7
Junior Miqueletti <juniormiqueletti@gmail.com>
2016-11-02 16:22:36 -0200

    Format JSON output with @JsonInclude from jackson

--- src/main/java/br/com/juniormiqueletti/socialbooks/domain/Book.java
+++ src/main/java/br/com/juniormiqueletti/socialbooks/domain/Book.java
@@ -1,15 +1,31 @@
 package br.com.juniormiqueletti.socialbooks.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.Date;
 import java.util.List;
 
 public class Book {
+
+    @JsonInclude(Include.NON_NULL)
     private Long Id;
+
     private String name;
+
+    @JsonInclude(Include.NON_NULL)
     private Date publication;
+
+    @JsonInclude(Include.NON_NULL)
     private String publishingCompany;
+
+    @JsonInclude(Include.NON_NULL)
     private String summary;
+
+    @JsonInclude(Include.NON_NULL)
     private String author;
+
+    @JsonInclude(Include.NON_NULL)
     private List<Comment> comments;
 
     public Book(String name) {
