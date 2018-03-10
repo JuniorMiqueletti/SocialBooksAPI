fc2237ae41894b6a02a46ff98000bc54ace81802
Junior Miqueletti <junior.miqueletti@matera.com>
2018-03-09 22:37:21 -0300

    Make tests run with spring boot

--- src/test/java/br/com/juniormiqueletti/socialbooks/controller/AuthorControllerTest.java
+++ src/test/java/br/com/juniormiqueletti/socialbooks/controller/AuthorControllerTest.java
@@ -1,19 +1,28 @@
 package br.com.juniormiqueletti.socialbooks.controller;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.*;
 
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AuthorControllerTest {
 	
-	private static final String AUTHOR_URL = "http://localhost:8080/author";
+	private static final String AUTHOR_URL = "/author";
 	private static final int STATUS_CODE_UNAUTHORIZED = 401;
 	private static final int STATUS_CODE_OK = 200;
+	
+	@LocalServerPort
+    int port;
 
 	@Test
 	public void getWithoutAuthentication() {
 		given()
+			.port(port)
 			.get(AUTHOR_URL)
 		.then()
 			.statusCode(STATUS_CODE_UNAUTHORIZED);
@@ -22,6 +31,7 @@ public class AuthorControllerTest {
 	@Test
 	public void getWithAuthentication() {
 		given()
+			.port(port)
 			.auth()
 				.basic("juniormiqueletti", "p4ssw0rd")
 			.get(AUTHOR_URL)
