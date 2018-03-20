f8c3a164ad2b4bb843ce956acf438f4611609468
Junior Miqueletti <junior.miqueletti@matera.com>
2018-03-20 07:45:54 -0300

    Renaming test classes

--- src/test/java/br/com/juniormiqueletti/socialbooks/controller/AuthorControllerIT.java
+++ src/test/java/br/com/juniormiqueletti/socialbooks/controller/AuthorControllerIT.java
@@ -1,11 +1,11 @@
 package br.com.juniormiqueletti.socialbooks.controller;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,25 +14,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.juniormiqueletti.socialbooks.domain.Author;
-import br.com.juniormiqueletti.socialbooks.domain.Book;
 import io.restassured.response.Response;
-
-import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class BookControllerTest {
+public class AuthorControllerIT {
 	
-	private static final String BOOK_URL = "/book";
+	private static final String AUTHOR_URL = "/author";
 	private static final int STATUS_CODE_UNAUTHORIZED = 401;
 	private static final int STATUS_NOT_FOUND = 404;
 	private static final int STATUS_CODE_OK = 200;
 	private static final int STATUS_CREATED = 201;
-	private static final int STATUS_NO_CONTENT = 204;
 	private static final String CONTENT_TYPE_HEADER = "Content-Type";
 	private static final String LOCATION_HEADER = "location";
 	private static final String APPLICATION_JSON = "application/json";
-	static final String LOCATION_PATTERN = ".*"+ BOOK_URL + "/[0-9]+";
+	static final String LOCATION_PATTERN = ".*"+ AUTHOR_URL + "/[0-9]+";
 	
 	private static final String BASIC_AUTH_USER = "juniormiqueletti";
 	private static final String BASIC_AUTH_PASS = "p4ssw0rd";
@@ -44,7 +40,7 @@ public class BookControllerTest {
 	public void getWithoutAuthenticationTest() {
 		given()
 			.port(port)
-			.get(BOOK_URL)
+			.get(AUTHOR_URL)
 		.then()
 			.statusCode(STATUS_CODE_UNAUTHORIZED);
 	}
@@ -55,59 +51,63 @@ public class BookControllerTest {
 			.port(port)
 			.auth()
 				.basic(BASIC_AUTH_USER, BASIC_AUTH_PASS)
-			.get(BOOK_URL)
+			.get(AUTHOR_URL)
 		.then()
 			.statusCode(STATUS_CODE_OK);
 	}
-
+	
 	@Test
 	public void getNotFoundExceptionHandlerTest() {
 		given()
 			.port(port)
 			.auth()
 				.basic(BASIC_AUTH_USER, BASIC_AUTH_PASS)
-			.get(BOOK_URL + "/333")
+			.get(AUTHOR_URL + "/333")
 		.then()
 			.statusCode(STATUS_NOT_FOUND)
-			.body("title", equalTo("The book has not been found!"))
+			.body("title", equalTo("The author has not been found!"))
 			.body("developerMessage", equalTo("http://error.socialbooks.com/404"));
 	}
-	
+
 	@Test
 	public void postAuthorTest() throws ParseException {
 		
-		Book book = createBookSample();
+		Author author = new Author();
+		author.setName("Junior");
+		author.setNationality("Unknown");
 		
 		Response response = 
 				given()
 				  .port(port)
 				  .auth()
 				    .basic(BASIC_AUTH_USER, BASIC_AUTH_PASS)
-			      .body(book)
+			      .body(author)
 			      .header(CONTENT_TYPE_HEADER, APPLICATION_JSON)
-			    .post(BOOK_URL)
+			    .post(AUTHOR_URL)
 				    .andReturn();
 
 		int statusCode = response.getStatusCode();
 		String headerLocation = response.getHeader(LOCATION_HEADER);
 
-		assertEquals(STATUS_CREATED, statusCode);
+		assertEquals(statusCode, STATUS_CREATED);
 		assertTrue(headerLocation.matches(LOCATION_PATTERN));
 	}
-
+	
 	@Test
 	public void postAuthorSmokeTest() throws ParseException {
 		
-		Book book = createBookSample();
+		Author author = new Author();
+		author.setName("Junior");
+		author.setNationality("Unknown");
 		
 		Response response = 
 				given()
 					.port(port)
 					.auth()
 						.basic(BASIC_AUTH_USER, BASIC_AUTH_PASS)
-					.body(book)
+					.body(author)
 						.header(CONTENT_TYPE_HEADER, APPLICATION_JSON)
-				.post(BOOK_URL)
+				.post(AUTHOR_URL)
 					.andReturn();
 		
 		int statusCode = response.getStatusCode();
@@ -127,67 +127,9 @@ public class BookControllerTest {
 		.extract()
 		.response();
 		
-		Book bookResponse = getResponse.body().as(Book.class);
+		Author authorResponse = getResponse.body().as(Author.class);
 		
-		assertEquals(book.getName(), bookResponse.getName());
-		assertEquals(book.getPublishingCompany(), bookResponse.getPublishingCompany());
-	}
-	
-	@Test
-	public void deleteNotFoundExceptionHandlerTest() {
-		given()
-			.port(port)
-			.auth()
-				.basic(BASIC_AUTH_USER, BASIC_AUTH_PASS)
-			.delete(BOOK_URL + "/333")
-		.then()
-			.statusCode(STATUS_NOT_FOUND)
-			.body("title", equalTo("The book has not been found!"))
-			.body("developerMessage", equalTo("http://error.socialbooks.com/404"));
-	}
-	
-	@Test
-	public void deleteTest() throws ParseException {
-		
-		Book book = createBookSample();
-		
-		Response response = 
-				given()
-					.port(port)
-					.auth()
-						.basic(BASIC_AUTH_USER, BASIC_AUTH_PASS)
-					.body(book)
-						.header(CONTENT_TYPE_HEADER, APPLICATION_JSON)
-				.post(BOOK_URL)
-					.andReturn();
-		
-		int statusCode = response.getStatusCode();
-		String headerLocation = response.getHeader(LOCATION_HEADER);
-		
-		assertEquals(statusCode, STATUS_CREATED);
-		assertTrue(headerLocation.matches(LOCATION_PATTERN));
-		
-		given()
-			.port(port)
-			.auth()
-				.basic(BASIC_AUTH_USER, BASIC_AUTH_PASS)
-			.delete(headerLocation)
-		.then()
-			.statusCode(STATUS_NO_CONTENT);
-	}
-	
-	private Book createBookSample() throws ParseException {
-		Author author = new Author();
-		author.setId(1L);
-		author.setName("Junior");
-		author.setNationality("Unknown");
-		
-		Book book = new Book();
-		book.setAuthor(author);
-		book.setName("Book of Life");
-		book.setPublication(new SimpleDateFormat("dd/mm/yyyy").parse("19/03/2018"));
-		book.setPublishingCompany("Miqueletti Sa");
-		book.setSummary("Book with resume of my life");
-		return book;
+		assertEquals(author.getName(), authorResponse.getName());
+		assertEquals(author.getNationality(), authorResponse.getNationality());
 	}
 }
