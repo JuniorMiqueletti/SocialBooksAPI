80d751ce8d49b5a8685942138beceb77b4fb6ff0
Junior Miqueletti <juniormiqueletti@gmail.com>
2018-11-26 22:36:58 -0200

    Change application to Web Application(SpringBootServletInitializer)

--- src/main/java/br/com/juniormiqueletti/socialbooks/App.java
+++ src/main/java/br/com/juniormiqueletti/socialbooks/App.java
@@ -2,10 +2,19 @@ package br.com.juniormiqueletti.socialbooks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
+
     public static void main(String[] args){
         SpringApplication.run(App.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(getClass());
     }
 }
